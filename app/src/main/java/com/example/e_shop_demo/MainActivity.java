04346ca6775b7cc6027e08b0621409fa3c67329d
PKS <pkumarsaha71@gmail.com>
2019-10-08 20:22:15 +0600

    update

--- app/src/main/java/com/example/e_shop_demo/MainActivity.java
+++ app/src/main/java/com/example/e_shop_demo/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.e_shop_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
 
         mainLoginbtn = findViewById(R.id.mainLoginbtn);
         joinNowbtn = findViewById(R.id.joinNowbtn);
+
+        mainLoginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, LogInActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
