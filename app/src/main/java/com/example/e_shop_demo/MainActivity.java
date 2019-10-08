2444475038f0b5c92d8a4b8291313549dca7f0cc
PKS <pkumarsaha71@gmail.com>
2019-10-08 17:28:45 +0600

    update

--- app/src/main/java/com/example/e_shop_demo/MainActivity.java
+++ app/src/main/java/com/example/e_shop_demo/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.e_shop_demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button joinNowbtn,mainLoginbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainLoginbtn = findViewById(R.id.mainLoginbtn);
+        joinNowbtn = findViewById(R.id.joinNowbtn);
+
     }
 }
