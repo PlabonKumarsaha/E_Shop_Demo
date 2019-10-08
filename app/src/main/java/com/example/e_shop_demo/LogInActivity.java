03c8e83e7f379708b4a77d00d880effcacf97138
PKS <pkumarsaha71@gmail.com>
2019-10-08 18:34:30 +0600

    update

---
+++ app/src/main/java/com/example/e_shop_demo/LogInActivity.java
@@ -0,0 +1,16 @@
+package com.example.e_shop_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogInActivity extends AppCompatActivity {
+
+   // https://github.com/rey5137/material
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
