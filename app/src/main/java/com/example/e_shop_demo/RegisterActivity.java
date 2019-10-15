cbe72b19b9363f1218c70dbd1e7fd1a4715af8c4
PKS <pkumarsaha71@gmail.com>
2019-10-16 00:43:56 +0600

    update

--- app/src/main/java/com/example/e_shop_demo/RegisterActivity.java
+++ app/src/main/java/com/example/e_shop_demo/RegisterActivity.java
@@ -1,8 +1,10 @@
 package com.example.e_shop_demo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -10,6 +12,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -70,6 +82,55 @@ public class RegisterActivity extends AppCompatActivity {
         }
     }
 
-    private void validatePhoneNo(String name, String phoneNo, String password) {
+    private void validatePhoneNo(final String name, final String phoneNo, final String password) {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(!(dataSnapshot.child("User").child(phoneNo).exists()))
+                {
+                    HashMap<String,Object>userDataMap = new HashMap<>();
+                    userDataMap.put("Phone No",phoneNo);
+                    userDataMap.put("UserName",name);
+                    userDataMap.put("Password",password);
+                    RootRef.child("User").child(phoneNo).updateChildren(userDataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if(task.isSuccessful())
+                            {
+                                Toast.makeText(getApplicationContext(),"Suecssfully created new account!",Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+
+                                Intent intent = new Intent(RegisterActivity.this,LogInActivity.class);
+                                startActivity(intent);
+                            }
+                            else {
+                                loadingBar.dismiss();
+                                Toast.makeText(getApplicationContext(),"UnSuecssfully,please try again!",Toast.LENGTH_SHORT).show();
+
+                            }
+
+                        }
+                    });
+
+
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"This"+phoneNo+" already has account",Toast.LENGTH_SHORT).show();
+                loadingBar.dismiss();
+                    Toast.makeText(getApplicationContext(),"Try another phone no",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
