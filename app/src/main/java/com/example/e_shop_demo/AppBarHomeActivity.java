2be5d99421110b0df0860d983094c92f77f994b2
PKS <pkumarsaha71@gmail.com>
2019-10-19 00:46:32 +0600

    update

---
+++ app/src/main/java/com/example/e_shop_demo/AppBarHomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.e_shop_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppBarHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_bar_home);
+    }
+}
---
+++ app/src/main/java/com/example/e_shop_demo/NavHeaderHomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.e_shop_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NavHeaderHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav_header_home);
+    }
+}
