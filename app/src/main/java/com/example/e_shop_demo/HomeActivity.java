3da6bde53ca652faf8b918320e0d177f5e47d9b3
PKS <pkumarsaha71@gmail.com>
2019-10-18 15:16:41 +0600

    update

--- app/src/main/java/com/example/e_shop_demo/HomeActivity.java
+++ app/src/main/java/com/example/e_shop_demo/HomeActivity.java
@@ -2,36 +2,15 @@ package com.example.e_shop_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import io.paperdb.Paper;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Button logoutBtn;
+    //https://github.com/hdodenhof/CircleImageView was used for circle image
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        logoutBtn = findViewById(R.id.logoutBtn);
-
-        logoutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this,MainActivity.class);
-                startActivity(intent);
-
-                //if we use the remeber me check box that will store the username and pass but as soon as we click log out that will clearup the username and password
-                //that were previously stored!
-
-                Paper.book().destroy();
-            }
-        });
     }
-
 }
